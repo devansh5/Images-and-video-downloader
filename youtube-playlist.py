d6a0933670c8d5ca6f58450bcb6d92dc7c50bfc1
DEvansh <43261186+devansh5@users.noreply.github.com>
2019-06-07 12:02:03 +0530

    downloading youtube playlist videos

--- youtube-playlist.py
+++ youtube-playlist.py
@@ -1,1 +1,10 @@
+from pytube import Playlist 
+pl=Playlist("#link of the playlist")
+print('Number of videos in playlist: %s' % len(pl.video_urls))
+pl.download_all()
 
+
+
+#to download for a specific directory 
+
+pl.download_all('/path/to/directory/')
